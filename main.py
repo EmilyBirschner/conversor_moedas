6a6d3cd4a547ec7a604a73d0d874a6bae289e89e
Emily <emilyb.oliveira@hotmail.com>
2026-01-31 16:22:59 -0300

    Melhoria do Json como resposta

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def converter(coin_origin, coin_target):
     )
     if response.status_code == 404:
         return response.json().get("message")
-    return response.json()
+    return response.json().get(f"{coin_origin}{coin_target}").get("bid")
 
 
 print(converter("BTC", "BRL"))
